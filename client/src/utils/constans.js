acd4ea9da64302920261338da21ced64ffaca41c
Cún🐶 <admin@MacBook-Pro.local>
2023-01-25 00:25:21 +0900

    update server url

--- client/src/utils/constans.js
+++ client/src/utils/constans.js
@@ -1,2 +1,2 @@
-export const GRAPHQL_SERVER = 'http://localhost:4000'
-export const GRAPHQL_SUBSCRIPTION_ENDPOINT = 'wss://localhost:4000/graphql'
+export const GRAPHQL_SERVER = 'hhttps://note-app-cxm8.onrender.com'
+export const GRAPHQL_SUBSCRIPTION_ENDPOINT = 'wss://note-app-cxm8.onrender.com/graphql'
